724e01e3753604530a91817da51ea83e2ca8b87f
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2024-03-18 13:32:12 -0300

    Trying ngrok deploy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
     {
         context: ['/nest-api'],
-        target: 'https://949b-189-57-151-123.ngrok-free.app/',
+        target: 'https://949b-189-57-151-123.ngrok-free.app',
         secure: false,
         logLevel: 'debug',
         pathRewrite: { '^/nest-api': '' }
